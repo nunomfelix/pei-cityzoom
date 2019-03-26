e074b5e52f85a3bb1c526dd9c9e24086a65a66a1
Lucas Seabra <lucasseabra@ua.pt>
2019-03-26 01:30:22 +0000

    Gateway added. Minor bugs fixed. Starting validation.

--- data_layer/index.js
+++ data_layer/index.js
@@ -1,6 +1,6 @@
+
 // npm modules
 const express = require('express')
-
 // my modules
 const parser = require('./parser')
 const producer = require('./kafka-producer')
@@ -9,36 +9,5 @@ const app = express()
 
 app.use(express.json())
 
-app.post('/czb/stream', (req,res) => {
-    console.log(req.body)
-    res.send({
-        status: 'create stream OK'
-    })
-
-})
-
-app.get('/czb/stream', (req,res) => {
-    console.log(req)
-    res.send({
-        status: 'read data in stream OK'
-    })
-})
-
-app.put('/czb/stream', (req,res) => {
-    console.log(req)
-    res.send({
-        status: 'put data in stream OK'
-    })
-})
-
-app.delete('/czb/stream', (req, res) => {
-    console.log(req)
-    res.send({
-        status: 'delete stream OK'
-    })
-})
-
-
-app.listen(8001, () => { 
-    console.log('listen in port 8001') 
-})
+app.use('/czb/stream', parser)
+app.listen(8001, () => { console.log('listen in port 8001') })
--- data_layer/parser.js
+++ data_layer/parser.js
@@ -1,5 +1,6 @@
 const joi = require('joi')
-
+const express = require('express')
+const  router = express.Router()
 /*
  * API Endpoints:
  * create stream         -> localhost:8000/czb/stream (POST)  
@@ -10,11 +11,41 @@ const joi = require('joi')
  * 
  */
 
+router.post('/', (req,res) => {
+    console.log(req.body)
+    res.send({
+        status: 'create stream OK'
+    })
 
-const parseRequest = () => {
+})
 
+router.get('/', (req,res) => {
+    console.log(req)
+    res.send({
+        status: 'read data in stream OK'
+    })
+})
+
+router.put('/', (req,res) => {
+    console.log(req)
+    res.send({
+        status: 'put data in stream OK'
+    })
+})
+
+router.delete('/', (req, res) => {
+    console.log(req)
+    res.send({
+        status: 'delete stream OK'
+    })
+})
+
+
+function validateCreate(stream){
+    //token,name,type
+    // const schema = { name: Joi.string().min(4).required(),
+    //                  token: x
+    //                  type: }
+    return Joi.validate(stream,schema)
 }
-
-module.exports = {
-    parseRequest
-}
+module.exports = router
