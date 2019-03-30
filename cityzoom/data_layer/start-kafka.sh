3a8d8db18becce70a6dd1c65c50aa5c6d2f59ed4
ZePaiva <zmppaiva99@gmail.com>
2019-03-30 02:28:21 +0000

    added a ton of scripts

--- cityzoom/data_layer/start-kafka.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-bin/zookeeper-server-start.sh configs/zookeeper.properties &
-bin/kafka-server-start.sh configs/server_0.properties &
-bin/kafka-server-start.sh configs/server_1.properties &
-bin/kafka-server-start.sh configs/server_2.properties &
-bin/kafka-server-start.sh configs/server_3.properties &
-bin/kafka-server-start.sh configs/server_4.properties &
