131dd686fbc5a9daa073fa5bd088512982fe5fc8
ZePaiva <zmppaiva99@gmail.com>
2019-04-03 00:10:48 +0100

    cenas pro mourato

--- cityzoom/data_layer/src/index.js
+++ cityzoom/data_layer/src/index.js
@@ -10,6 +10,7 @@ const parser = require('./parser')
 const app = express()
 app.use(express.json())
 
+mongoose.connect('mongodb://localhost/city_zoom_data_layer', {useNewUrlParser: true})
     .then(() => {
         console.log('Connected to MongoDB...')
         fetch(config.get('FETCHING_PERIOD'))
@@ -18,13 +19,6 @@ app.use(express.json())
 
 app.use('/czb/stream', parser.router)
 
-const connectionURL = 'mongodb://127.0.0.1:27017/'
-const databaseName = 'city_zoom_service_layer'
-
-mongoose.connect(connectionURL + databaseName, {
-    useNewUrlParser: true,
-    useCreateIndex: true
-}, () => mongooseDebug("Connected to mongo database!"))
 
 app.listen(8001, () => {
     console.log('listen in port 8001')
