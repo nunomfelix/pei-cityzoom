7e9f846042163e37b9791041963180b699cd8b34
andremourato <andremourato@ua.pt>
2019-04-02 17:38:24 +0000

    Fixed push data into stream in data layer

--- cityzoom/data_layer/src/parser.js
+++ cityzoom/data_layer/src/parser.js
@@ -7,7 +7,7 @@
  * delete stream         -> localhost:8000/czb/stream (DELETE)
  * 
  */
-const {run,genDataCreationPayload}= require('./kafka-producer')
+const { run, genDataCreationPayload } = require('./kafka-producer')
 const create = require('./kafka-admin')
 const prod = require('./kafka-producer')
 const consumer = require('./kafka-consumer')
@@ -17,116 +17,116 @@ const express = require('express')
 const router = express.Router()
 
 const streamSchema = new mongoose.Schema({
-    name:{
+    name: {
         type: String,
         required: true,
         minlength: 5,
         maxlength: 30
     },
-    description:{
+    description: {
         type: String,
         minlength: 5,
         maxlength: 200
     },
-    mobile:{
+    mobile: {
         isMobile: Boolean,
     },
-    type:{
+    type: {
         type: String,
         required: true,
         minlength: 4,
         maxlength: 30
     },
-    ttl:{
+    ttl: {
         type: Number,
-         min: 10,
-         max: 200
+        min: 10,
+        max: 200
     },
-    periodicity:{
+    periodicity: {
         type: Number,
-         min: 10,
-         max: 200
+        min: 10,
+        max: 200
     },
     timestamp: {
-         type: Number
+        type: Number
     }
 })
 
-const Stream = mongoose.model('Stream',streamSchema)
+const Stream = mongoose.model('Stream', streamSchema)
 
-router.post('/', async (req,res) => {
-    const {error} = validateCreate(req.body);
-    if(error) return res.status(400).send(error.details[0].message);
+router.post('/', async (req, res) => {
+    const { error } = validateCreate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
     //temp
     var account = 'user_1'
     //kafka
 
     await create.createStream(req.body.name)
-   
+
     console.log(req.body)
-   
+
     let stream = new Stream(req.body)
-   
+
     stream = await stream.save()
-    .then((result) => {
-        console.log('saved: ', result)    
-    }).catch((err) => {
-        console.log('error: ', err)
-    });
-   
-    if( req.body.periodicity !=0){
-        req.body.periodicity 
+        .then((result) => {
+            console.log('saved: ', result)
+        }).catch((err) => {
+            console.log('error: ', err)
+        });
+
+    if (req.body.periodicity != 0) {
+        req.body.periodicity
     }
     else {
         req.body.periodicity = 1200
     }
-    
+
     res.send({
-        status:'read data in stream OK' ,       
+        status: 'Created data stream',
         name: req.body.name,
         account,
-        periodicity:req.body.periodicity 
+        periodicity: req.body.periodicity
     })
-    
+
 })
 //query string
 
-router.get('/values', async (req,res) => {
-  
+router.get('/values', async (req, res) => {
+
     //const {error} = validateQueryString(req.body);
-   // if(error) return res.status(400).send(error.details[0].message);
+    // if(error) return res.status(400).send(error.details[0].message);
 
     const query = await Stream
         .find(req.query)
-    
+
     console.log('caralho')
     console.log(query.name)
-    
+
     const c = consumer.readData(query.name)
-    
+
     console.log(c)
     //console.log(req.query) 
     res.send(
-        console.log(query) 
+        console.log(query)
     )
 })
 
-router.get('/', (req,res) => {
+router.get('/', (req, res) => {
     //console.log(req.query)
     res.send({
         status: 'read data in stream OK'
     })
 })
 
-router.put('/',async (req,res) => {
-    const {error} = validatePutData(req.body);
-    if(error) return res.status(400).send(error.details[0].message);
+router.put('/', async (req, res) => {
+    const { error } = validatePutData(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
 
-    const stream_name = await Stream.findOneAndUpdate(req.body.name)
+    const stream_name = await Stream.findOneAndUpdate(req.body.stream_name)
 
-    if(!stream_name) return res.status(404).send('The stream with the given name was not found');
-    
-    var payload = prod.genDataCreationPayload('user_1', req.body.name, req.body.value, Number(new Date()), req.body.location)
+    if (!stream_name) return res.status(404).send('The stream with the given name was not found');
+
+    var payload = prod.genDataCreationPayload('user_1', req.body.stream_name, req.body.value, Number(new Date()), req.body.location)
     prod.putData(payload)
 
     //console.log(req)
@@ -138,7 +138,7 @@ router.put('/',async (req,res) => {
 
 router.delete('/', async (req, res) => {
     const stream_name = await Stream.findOneAndDelete(req.body.name)
-    if(!stream_name) return res.status(404).send('The stream with the given name was not found');
+    if (!stream_name) return res.status(404).send('The stream with the given name was not found');
 
     console.log(req)
     res.send({
@@ -146,34 +146,36 @@ router.delete('/', async (req, res) => {
     })
 })
 
-function validateQueryString(strean){
-    const schema = { stream_name : Joi.string().min(4).required(),
-                     interval    : Joi.number()                
+function validateQueryString(strean) {
+    const schema = {
+        stream_name: Joi.string().min(4).required(),
+        interval: Joi.number()
     }
-    return Joi.validate(stream,schema) 
+    return Joi.validate(stream, schema)
 }
 
-function validateDataStream(stream){
-   const schema = { stream_name : Joi.string().min(4).required() }
-   return Joi.validate(stream,schema) 
+function validateDataStream(stream) {
+    const schema = { stream_name: Joi.string().min(4).required() }
+    return Joi.validate(stream, schema)
 }
-function validatePutData(stream){
-    const schema = { stream_name : Joi.string().min(4).required(),
-                     values     : Joi.string().min(4).required(),
-                     location   : Joi.array().items( Joi.number().required() , Joi.number().required())
+function validatePutData(stream) {
+    const schema = {
+        stream_name: Joi.string().min(4).required(),
+        values: Joi.string().min(4).required(),
+        location: Joi.array().items(Joi.number().required(), Joi.number().required())
     }
-    return Joi.validate(stream,schema)
+    return Joi.validate(stream, schema)
 }
 
-function validateCreate(stream){
-    const schema = { 
-        name:        Joi.string().min(4).required(),
-        type:        Joi.string().min(4).required(),
+function validateCreate(stream) {
+    const schema = {
+        name: Joi.string().min(4).required(),
+        type: Joi.string().min(4).required(),
         description: Joi.string(),
-        mobile:      Joi.boolean(),
+        mobile: Joi.boolean(),
         periodicity: Joi.number().integer().positive(),
-        ttl:         Joi.number().integer().positive()
+        ttl: Joi.number().integer().positive()
     }
-    return Joi.validate(stream,schema)
+    return Joi.validate(stream, schema)
 }
 module.exports = router
