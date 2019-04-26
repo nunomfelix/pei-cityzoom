fdf916fdf1da6893cb52dd3d6f60d5fba468697b
ZePaiva <zmppaiva99@gmail.com>
2019-04-26 04:52:26 +0100

    added jar files for execution on other machines

---
+++ cityzoom/DataLayer/DLAPI/src/main/java/API/DataLayerAPI.java
@@ -0,0 +1,39 @@
+package API;
+
+import API.Aux.MongoAux;
+import API.Middleware.Validation;
+import API.Routes.Streams;
+import API.Routes.Values;
+import API.Sinks.BackendSink;
+import com.mongodb.client.MongoCollection;
+import org.bson.Document;
+
+import static spark.Spark.*;
+
+public class DataLayerAPI {
+    public static BackendSink producer = new BackendSink();
+    public static Validation validator = new Validation();
+    public static MongoCollection<Document> streams = MongoAux.getCollection("streams");
+    public static MongoCollection<Document> values = MongoAux.getCollection("values");
+
+    public static void main(String[] args) {
+
+        // server configurations
+        port(8001);
+
+        // routes
+        path("/czb", () -> {
+            // stream routes and paths
+            post("/stream", Streams::createStream);
+            path("/stream", () -> {
+                get("/list", Streams::listStreams);
+                get("/:stream", Streams::detailStream);
+                delete("/:stream", Streams::deleteStream);
+            });
+
+            // values routes and paths
+            get("/values",  Values::getValues);
+            post("/values", Values::postValue);
+        });
+    }
+}
