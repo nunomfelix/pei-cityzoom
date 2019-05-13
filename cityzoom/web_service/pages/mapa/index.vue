3f1c3fd52d15308ead72bc67b4b10f7133cab5b6
Gonçalo <gvitor@ua.pt>
2019-05-13 19:59:36 +0100

    fasterMap

--- cityzoom/web_service/pages/mapa/index.vue
+++ cityzoom/web_service/pages/mapa/index.vue
@@ -18,7 +18,9 @@ export default {
         const source = require( 'ol/source');
         const layer = require( 'ol/layer');
         // const style = require( 'ol/style');
-        // const geom = require( 'ol/geom');
+
+        const format = require('ol/format')
+        const geom = require( 'ol/geom');
         // const {Polygon, fromExtent} = require( 'ol/geom/Polygon');
         const interaction = require( 'ol/interaction');
         // const extent = require( 'ol/extent');
@@ -26,22 +28,32 @@ export default {
         // const { Feature } = require( 'ol')
         // const { click, pointerMove, altKeyOnly, noModifierKeys, altShiftKeysOnly, platformModifierKeyOnly } = require( 'ol/events/condition.js');
 
-        const centerpos = [-8.6, 40.65];
+        const centerpos = [-8.661682, 40.6331731];
         const center = proj.transform(centerpos, 'EPSG:4326', 'EPSG:3857');
         const maxExtent = [-9.5, 37, -6.2, 42.5];
         //const maxExtent = [-180, -90, 180, 90];
+        var vector_source = new source.Vector({
+                projection : 'EPSG:3857',
+                url: 'portugal_municipios.geojson',
+                format: new format.GeoJSON()
+        });
 
         this.map = new Ol.Map({
             target: 'map',
             layers: [
                 new layer.Tile({
                     source: new source.OSM(),
-                })
+                }),
+                new layer.Vector({
+                    source: vector_source,
+                    renderMode: 'image'
+                })                
             ],
             view: new Ol.View({
-                zoom: 4,
+                zoom: 8,
                 center,
             })
+
         })
     }
 
