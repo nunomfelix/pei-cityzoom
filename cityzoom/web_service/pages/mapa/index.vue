fdc2f9ef8cbe6bd2cde6e2be02b2a8cda8eff9e5
Gonçalo <gvitor@ua.pt>
2019-05-13 21:22:18 +0100

    loading module

--- cityzoom/web_service/pages/mapa/index.vue
+++ cityzoom/web_service/pages/mapa/index.vue
@@ -1,14 +1,16 @@
 <template>
-<div class="mapMargin mapHeight">
-    <div class="mapHeight" id="map"></div>
-</div>
+    <div class="mapMargin mapHeight" style="position:relative">
+        <div class="mapHeight" id="map"></div>
+        <Loading :show="true" type="absolute"/> 
+    </div>
 </template>
 
 <script>
 export default {
     data() {
         return {
-            map: null
+            map: null,
+            loaded: false
         }
     },
     mounted() {
@@ -55,11 +57,112 @@ export default {
             })
 
         })
+
+        vector_source.on('change', () => {
+            if(vector_source.getState() == 'ready' && !this.loaded) {
+                this.loaded = true
+                console.log("asdfasdf")
+            }
+        })
     }
 
 }
 </script>
 
 <style>
-
+    .lds-roller-fixed {
+        position: fixed;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    }
+    .lds-roller-relative {
+        position: relative;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100%;
+    }
+    .lds-roller {
+        width: 64px;
+        height: 64px;
+    }
+    .lds-roller div {
+        animation: lds-roller 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+        transform-origin: 32px 32px;
+    }
+    .lds-roller div:after {
+    content: " ";
+        display: block;
+        position: absolute;
+        width: 8px;
+        height: 8px;
+        border-radius: 50%;
+        background: black;
+        margin: -3px 0 0 -3px;
+    }
+    .lds-roller div:nth-child(1) {
+        animation-delay: -0.036s;
+    }
+    .lds-roller div:nth-child(1):after {
+        top: 50px;
+        left: 50px;
+    }
+    .lds-roller div:nth-child(2) {
+        animation-delay: -0.072s;
+    }
+    .lds-roller div:nth-child(2):after {
+        top: 54px;
+        left: 45px;
+    }
+    .lds-roller div:nth-child(3) {
+        animation-delay: -0.108s;
+    }
+    .lds-roller div:nth-child(3):after {
+        top: 57px;
+        left: 39px;
+    }
+    .lds-roller div:nth-child(4) {
+        animation-delay: -0.144s;
+    }
+    .lds-roller div:nth-child(4):after {
+        top: 58px;
+        left: 32px;
+    }
+    .lds-roller div:nth-child(5) {
+        animation-delay: -0.18s;
+    }
+    .lds-roller div:nth-child(5):after {
+        top: 57px;
+        left: 25px;
+    }
+    .lds-roller div:nth-child(6) {
+        animation-delay: -0.216s;
+    }
+    .lds-roller div:nth-child(6):after {
+        top: 54px;
+        left: 19px;
+    }
+    .lds-roller div:nth-child(7) {
+        animation-delay: -0.252s;
+    }
+    .lds-roller div:nth-child(7):after {
+        top: 50px;
+        left: 14px;
+    }
+    .lds-roller div:nth-child(8) {
+        animation-delay: -0.288s;
+    }
+    .lds-roller div:nth-child(8):after {
+        top: 45px;
+        left: 10px;
+    }
+    @keyframes lds-roller {
+        0% {
+            transform: rotate(0deg);
+        }
+        100% {
+            transform: rotate(360deg);
+        }
+    }
 </style>
--- cityzoom/web_service/plugins/core-components.js
+++ cityzoom/web_service/plugins/core-components.js
@@ -5,6 +5,7 @@ import LoginForm from "@/components/Login";
 import SeriesGraph from "@/components/UI/Graphs/Series"
 import StackedBar from "@/components/UI/Graphs/Columns"
 import Line from "@/components/UI/Graphs/Line"
+import Loading from "@/components/UI/Loading"
 
 Vue.component("Navbar", Navbar);
 Vue.component("Header", Header);
@@ -12,3 +13,4 @@ Vue.component("LoginForm", LoginForm);
 Vue.component("SeriesGraph", SeriesGraph);
 Vue.component("StackedBar", StackedBar);
 Vue.component("LineGraph", Line)
+Vue.component("Loading", Loading)
