5810cc33e1faef89aa4e76164ba4516341a88e69
ZePaiva <zmppaiva99@gmail.com>
2019-05-23 19:13:43 +0100

    started new data_layer

--- cityzoom/DataLayer/Dev/data_up.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-xterm -e java -jar DevicesSinks-1.0-jar-with-dependencies.jar &
-xterm -e java -jar Sinks-1.0-jar-with-dependencies.jar &
-xterm -e java -jar ValuesSinks-1.0-jar-with-dependencies.jar &
-xterm -e java -jar DLAPI-1.0-jar-with-dependencies.jar &
--- cityzoom/DataLayer/Production/load_kafka.sh
+++
@@ -1,3 +0,0 @@
-kafka-topics.sh --zookeeper localhost:2181 --create --topic Values --partitions 3 --replication-factor 3
-kafka-topics.sh --zookeeper localhost:2181 --create --topic Streams --partitions 3 --replication-factor 3
-kafka-topics.sh --zookeeper localhost:2181 --create --topic Alarms --partitions 3 --replication-factor 3
