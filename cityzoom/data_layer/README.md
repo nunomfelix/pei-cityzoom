c0c0c8ca4ee36115274011854dc2581df221c145
andremourato <andremourato@ua.pt>
2019-05-24 19:11:32 +0100

    Initial broker commit

---
+++ cityzoom/data_layer/README.md
@@ -0,0 +1,13 @@
+#QUICKSTART
+
+- Run mongodb (sudo service mongod start)
+
+- In folder cityzoom/data_layer/ run the following command:
+```sh
+$ npm run dev
+```
+- If anything goes wrong with the above command run the following commands in two different terminals:
+ ```sh
+$ npm run broker
+$ npm run index
+```
---
+++ cityzoom/data_layer/src/broker.js
@@ -0,0 +1,53 @@
+var mosca = require('mosca');
+var colors = require('colors')
+const clientDebug = require('debug')('broker:Client')
+const pubDebug = require('debug')('broker:Publish')
+const subDebug = require('debug')('broker:Publish')
+
+var listener = {
+  //using listener
+  type: 'mongo',
+  url: 'mongodb://localhost:27017/mqtt',
+  pubsubCollection: 'pubsub',
+  mongo: {}
+};
+
+var settings = {
+  port: 1883,
+  host: 'localhost',
+  backend: listener
+};
+
+var server = new mosca.Server(settings);
+
+//When a client connects to the broker
+server.on('clientConnected', function(client) {
+  clientDebug('Client connected: '+ colors.yellow(client.id));
+});
+ 
+//When a client disconnects to the broker
+server.on('clientDisconnected', function(client) {
+  clientDebug('Client disconnected: '+ colors.yellow(client.id));
+});
+
+// fired when a message is published
+server.on('published', function(packet, client) {
+  subDebug('Published in topic', colors.blue(packet.topic), 'payload',colors.red(packet.payload.toString('utf8')));
+});
+
+// fired when a topic is subscribed
+server.on('subscribed', function(topic, client) {
+  subDebug('Client', colors.yellow(client.id),'subscribed topic', colors.blue(topic));
+});
+
+// fired when a topic is subscribed
+server.on('unsubscribed', function(topic, client) {
+  subDebug('Client', colors.yellow(client.id),'unsubscribed topic', colors.blue(topic));
+});
+
+server.on('ready', setup);
+ 
+// fired when the mqtt server is ready
+function setup() {
+  console.log('Mosca server is up and running');
+}
