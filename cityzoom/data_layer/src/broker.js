5874744cdc072ef22ac82052a4ae2e67e2614400
andremourato <andremourato@ua.pt>
2019-05-24 19:30:53 +0100

    Added port to log and updated package.json

--- cityzoom/data_layer/src/broker.js
+++ cityzoom/data_layer/src/broker.js
@@ -49,5 +49,5 @@ server.on('ready', setup);
  
 // fired when the mqtt server is ready
 function setup() {
-  console.log('Mosca server is up and running');
+  console.log('Mosca server is up and running in port '+settings.port);
 }
