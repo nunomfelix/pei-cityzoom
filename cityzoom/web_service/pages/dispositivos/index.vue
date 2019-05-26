ea5884dc6b6aa42a61a187693b2fd6b28f744249
Nuno Félix <skidrush@live.com>
2019-05-26 18:35:36 +0100

    ..

--- cityzoom/web_service/pages/dispositivos/index.vue
+++ cityzoom/web_service/pages/dispositivos/index.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="mainMargin fontSize" >
     <b-table :items="items" :fields="fields" :tbody-tr-class="rowClass">
+      <template slot="streams" slot-scope="data">
+        <div class="img">
+          {{ data.value }} <img @click="tableImage()" src="icons/Temperature.png" style="width:25px">
+        </div>
+      </template>
       <template slot="location" slot-scope="data">
-        
         <div class="img">
           {{ data.value }} <img @click="tableImage()" src="icons/search.png">
-
         </div>
       </template>
     </b-table>
@@ -18,10 +21,10 @@
       return {
         fields: ['device_id', 'device_status', 'streams','location'],
         items: [
-          { device_id: 1, device_status: 'Operational', streams: 'CO2, Temperature' , location: 'Aveiro' , _cellVariants: { device_status: 'success' }},
-          { device_id: 2, device_status: 'Operational', streams: 'CO2, Temperature', location: 'Aveiro'  ,_cellVariants: { device_status: 'success' }},
-          { device_id: 3, device_status: 'Operational', streams: 'CO2, Temperature', location: 'Aveiro'  ,_cellVariants: { device_status: 'success' }},
-          { device_id: 4, device_status: 'Operational', streams: 'CO2, Temperature', location: 'Aveiro ' ,_cellVariants: { device_status: 'success' }},
+          { device_id: 1, device_status: 'Operational', streams: 'Temperature' , location: 'Aveiro' , _cellVariants: { device_status: 'success' }},
+          { device_id: 2, device_status: 'Dead', streams: 'Temperature', location: 'Aveiro'  ,_cellVariants: { device_status: 'danger' }},
+          { device_id: 3, device_status: 'Operational', streams: 'Temperature', location: 'Aveiro'  ,_cellVariants: { device_status: 'success' }},
+          { device_id: 4, device_status: 'Operational', streams: 'Temperature', location: 'Aveiro ' ,_cellVariants: { device_status: 'success' }},
         ]
       }
     },
