178e7e715cb69d57bc57866fae94da91673dbac9
Nuno Félix <skidrush@live.com>
2019-05-26 19:11:51 +0100

    alteração nas streams tabela

--- cityzoom/web_service/pages/dispositivos/index.vue
+++ cityzoom/web_service/pages/dispositivos/index.vue
@@ -3,7 +3,12 @@
     <b-table :items="items" :fields="fields" :tbody-tr-class="rowClass">
       <template slot="streams" slot-scope="data">
         <div class="img">
-          {{ data.value }} <img @click="tableImage()" src="icons/Temperature.png" style="width:25px">
+          {{ data.value }} 
+          <img @click="tableImage()" src="icons/sensor.png" style="width:25px">
+          <img @click="tableImage()" src="icons/streams/humidity_stream.png" style="width:25px">
+          <img @click="tableImage()" src="icons/streams/ozone_stream.png" style="width:25px">
+          <img @click="tableImage()" src="icons/streams/pressure_stream.png" style="width:25px">
+
         </div>
       </template>
       <template slot="location" slot-scope="data">
@@ -21,10 +26,10 @@
       return {
         fields: ['device_id', 'device_status', 'streams','location'],
         items: [
-          { device_id: 1, device_status: 'Operational', streams: 'Temperature' , location: 'Aveiro' , _cellVariants: { device_status: 'success' }},
-          { device_id: 2, device_status: 'Dead', streams: 'Temperature', location: 'Aveiro'  ,_cellVariants: { device_status: 'danger' }},
-          { device_id: 3, device_status: 'Operational', streams: 'Temperature', location: 'Aveiro'  ,_cellVariants: { device_status: 'success' }},
-          { device_id: 4, device_status: 'Operational', streams: 'Temperature', location: 'Aveiro ' ,_cellVariants: { device_status: 'success' }},
+          { device_id: 1, device_status: 'Operational', streams: '' , location: 'Aveiro' , _cellVariants: { device_status: 'success' }},
+          { device_id: 2, device_status: 'Dead', streams: '', location: 'Aveiro'  ,_cellVariants: { device_status: 'danger' }},
+          { device_id: 3, device_status: 'Operational', streams: '', location: 'Aveiro'  ,_cellVariants: { device_status: 'success' }},
+          { device_id: 4, device_status: 'Operational', streams: '', location: 'Aveiro ' ,_cellVariants: { device_status: 'success' }},
         ]
       }
     },
