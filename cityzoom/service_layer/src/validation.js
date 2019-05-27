57b008d29ecbc8cacbb89989132dbb4fa24101e5
andremourato <andremourato@ua.pt>
2019-05-27 03:42:22 +0100

    Validated all device endpoints in service layer

--- cityzoom/service_layer/src/validation.js
+++ cityzoom/service_layer/src/validation.js
@@ -66,7 +66,7 @@ function validateCreateDevice(object) {
         description: Joi.string().optional(),
         vertical: Joi.string().required(),
         mobile: Joi.boolean().required(),
-        municipality: joi.string().optional(),
+        municipality: Joi.string().optional(),
         provider: Joi.string().required()
     })
     return Joi.validate(object, schema)
