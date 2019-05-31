10d5df48914cdcd2ac5a9da888602c43025973be
andremourato <andremourato@ua.pt>
2019-05-31 01:09:42 +0000

    Added Battery and location of the device and cityzoom logo

--- mobile-app/app/src/main/java/com/example/cityzoomapp/MainActivity.java
+++ mobile-app/app/src/main/java/com/example/cityzoomapp/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.cityzoomapp;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.BatteryManager;
 import android.os.Bundle;
 import android.os.PowerManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.util.Log;
@@ -19,26 +31,33 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     Sensor proximitySensor;
     TextView proximityText;
+    TextView batteryTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //Toolbar toolbar = findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
+        //ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+        //        this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        //drawer.addDrawerListener(toggle);
+        //toggle.syncState();
         navigationView.setNavigationItemSelectedListener(this);
 
         //Sensors
@@ -46,7 +65,7 @@ public class MainActivity extends AppCompatActivity
                 (SensorManager) getSystemService(SENSOR_SERVICE);
         proximitySensor =
                 sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
-        if(proximitySensor == null) {
+        if (proximitySensor == null) {
             Log.e("app", "Proximity sensor not available.");
             finish(); // Close app
         }
@@ -56,10 +75,10 @@ public class MainActivity extends AppCompatActivity
         SensorEventListener proximitySensorListener = new SensorEventListener() {
             @Override
             public void onSensorChanged(SensorEvent sensorEvent) {
-                if(sensorEvent.values[0] < proximitySensor.getMaximumRange()) {
+                if (sensorEvent.values[0] < proximitySensor.getMaximumRange()) {
                     proximityText.setBackgroundColor(Color.RED);
                     //proximityText.setText("Somethings is close");
-                }else{
+                } else {
                     proximityText.setBackgroundColor(Color.GREEN);
                     //proximityText.setText("Nothing is close");
                 }
@@ -69,12 +88,77 @@ public class MainActivity extends AppCompatActivity
             public void onAccuracyChanged(Sensor sensor, int i) {
             }
         };
-
         // Register it, specifying the polling interval in
         // microseconds
         sensorManager.registerListener(proximitySensorListener,
                 proximitySensor, 2 * 1000 * 1000);
+
+        //Battery
+        batteryTxt = (TextView) this.findViewById(R.id.batteryTxt);
+        this.registerReceiver(this.mBatInfoReceiver, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+
+        //Coordinates
+        LocationManager locationManager = (LocationManager)
+                getSystemService(Context.LOCATION_SERVICE);
+        LocationListener locationListener = new MyLocationListener();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Log.d("app","No permission for location");
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION}, 101);
+        }
+        locationManager.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER, 100, 0, locationListener);
     }
+
+    /*---------- Listener class to get coordinates ------------- */
+    private class MyLocationListener implements LocationListener {
+
+        @Override
+        public void onLocationChanged(Location loc) {
+            String longitude = ""+loc.getLongitude();
+            String latitude = ""+loc.getLatitude();
+            TextView longTxt = (TextView) findViewById(R.id.longTxt);
+            TextView latTxt = (TextView) findViewById(R.id.latTxt);
+            longTxt.setText(longitude);
+            latTxt.setText(latitude);
+            Log.d("app","long:"+longitude);
+            Log.d("app","lat:"+latitude);
+
+            /*------- To get city name from coordinates -------- */
+            /*String cityName = null;
+            Geocoder gcd = new Geocoder(getBaseContext(), Locale.getDefault());
+            List<Address> addresses;
+            try {
+                addresses = gcd.getFromLocation(loc.getLatitude(),
+                        loc.getLongitude(), 1);
+                if (addresses.size() > 0) {
+                    System.out.println(addresses.get(0).getLocality());
+                    cityName = addresses.get(0).getLocality();
+                }
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            String s = longitude + "\n" + latitude + "\n\nMy Current City is: "
+                    + cityName;*/
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {}
+
+        @Override
+        public void onProviderEnabled(String provider) {}
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {}
+    }
+
+    private BroadcastReceiver mBatInfoReceiver = new BroadcastReceiver(){
+        @Override
+        public void onReceive(Context ctxt, Intent intent) {
+            int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
+            batteryTxt.setText(String.valueOf(level) + "%");
+        }
+    };
 
     @Override
     public void onBackPressed() {
@@ -88,8 +172,6 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
@@ -99,11 +181,6 @@ public class MainActivity extends AppCompatActivity
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
         return super.onOptionsItemSelected(item);
     }
